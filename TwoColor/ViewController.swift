1a200cdeb65c6d4092411f695e9828132ef38da9
Peter Rosenthal <peter.rosenthal@gmail.com>
2015-11-19 13:00:40 -0500

    Added TwoColors files

--- TwoColor/ViewController.swift
+++ TwoColor/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Bluesquare: UIImageView!
+    
+    @IBOutlet weak var Redsquare: UIImageView!
+    
+    @IBOutlet weak var BlueButton: UIButton!
+    
+    @IBOutlet weak var RedButton: UIButton!
+    
+    @IBOutlet weak var ResetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideBlueSquare(sender: AnyObject) {
+        Bluesquare.hidden = true
+        Redsquare.hidden = false
+    }
 
+    @IBAction func HideRedSquare(sender: AnyObject) {
+        Bluesquare.hidden = false
+        Redsquare.hidden = true
+    }
+    
+    @IBAction func ResetAll(sender: AnyObject) {
+        Bluesquare.hidden = false
+        Redsquare.hidden = false
+    }
 }
 
